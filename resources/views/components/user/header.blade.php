f88447d4c97e433c23f7d09386acb6fc7d60b93e
dianazepeda3 <90267006+dianazepeda3@users.noreply.github.com>
2023-03-31 16:24:10 -0600

    info pag clientes
    Se lleno la información sobre la empresa

--- resources/views/components/user/header.blade.php
+++ resources/views/components/user/header.blade.php
@@ -12,8 +12,7 @@
                 <li><a class="nav-link scrollto " href="#hero">Inicio</a></li>
                 <li><a class="nav-link scrollto" href="#about">Nosotros</a></li>
                 <li><a class="nav-link scrollto" href="#services">Servicios</a></li>
-                <li><a class="nav-link   scrollto" href="#portfolio">Eventos</a></li>
-                <li><a class="nav-link scrollto" href="#team">Team</a></li>            
+                <li><a class="nav-link   scrollto" href="#portfolio">Eventos</a></li>    
                 <li><a class="nav-link scrollto" href="#contact">Contacto</a></li>                                            
             @endif
             <li><a class="getstarted scrollto" href={{route('login')}}>Iniciar Sesión</a></li>
--- resources/views/parciales/nosotros.blade.php
+++ resources/views/parciales/nosotros.blade.php
@@ -9,22 +9,28 @@
     <div class="row content">
         <div class="col-lg-6">
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.
+            En Tatengo Eventos, nuestro compromiso es hacer que cada uno de nuestros clientes se sienta especial 
+            y único transformando cualquier espacio en un lugar mágico y espectacular.
+
+            Nuestro objetivo es hacer que cada evento sea inolvidable, dejando una huella duradera en la mente 
+            de cada uno de nuestros clientes.
         </p>
         <ul>
-            <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
-            <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-            <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+            <li><i class="ri-check-double-line"></i> Decoración de bodas y quinceañeras</li>
+            <li><i class="ri-check-double-line"></i> Eventos corporativos</li>
+            <li><i class="ri-check-double-line"></i> Fiestas temáticas</li>
+            <li><i class="ri-check-double-line"></i> Y mucho más...</li>
         </ul>
         </div>
         <div class="col-lg-6 pt-4 pt-lg-0">
         <p>
-            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Nosotros valoramos la relación con nuestros clientes y trabajamos de manera cercana y 
+            personalizada para garantizar que cada evento sea único y a medida. Nos comprometemos a hacer que 
+            cada uno de nuestros clientes se sienta especial y satisfecho con nuestros servicios.
+
+            Gracias por elegir Tatengo Eventos, esperamos trabajar juntos para hacer de su evento un éxito inolvidable.
         </p>
-        <a href="#" class="btn-learn-more">Learn More</a>
+        <!--<a href="#" class="btn-learn-more">Learn More</a>-->
         </div>
     </div>
 
